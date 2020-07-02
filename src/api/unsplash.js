13bc372d1f89aa64b53f711895a0b4c42ce3df92
EakzIT <pilpaff@gmail.com>
2020-07-02 18:13:45 +0300

    process of calibration the api and search

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,15 +1,28 @@
+import {useEffect, useState} from 'react';
+
 import fetch from 'isomorphic-fetch';
 const token = 'LxxnGmngRmOMt3UWBLFO-6u7WOjKY0_YqM2rKLX9PLA';
-class Unsplash {
-  static async search(searchTerm, token) {
-    const url = `https://api.unsplash.com/search/photos?page=1&query=${searchTerm}`;
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Client-ID ${token}`,
-      },
-    });
-    console.log(response);
-  }
-}
+export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-Unsplash.search('hi', token);
+  const searchApi = async searchTerm => {
+    try {
+      const url = `https://api.unsplash.com/search/photos?page=1&query=${searchTerm}`;
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Client-ID ${token}`,
+        },
+      });
+      const jsonify = await response.json();
+      console.log(jsonify.results.length);
+      setResults(jsonify);
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
+  };
+  useEffect(() => {
+    searchApi('job');
+  }, []);
+  return [searchApi, results, errorMessage];
+};
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import SearchBar from '../components/SearchBar';
+import Unsplash from '../api/unsplash';
 
 const HomeScreen = () => {
-  return <Text>Hi hi hi</Text>;
+  const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = Unsplash();
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => console.log(term)}
+      />
+      <Text>{results.length}</Text>
+    </View>
+  );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
 export default HomeScreen;
